f12cadcdb2e5cceea98273adb4f03082bc89f0fc
happy5205205 <30718405+happy5205205@users.noreply.github.com>
2018-12-07 17:39:48 +0800

    增加Fashion-mnist图象数据

--- PythonAI/ImageData/FashionMnist/main.py
+++ PythonAI/ImageData/FashionMnist/main.py
@@ -10,10 +10,11 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 from PythonAI.ImageData.FashionMnist import config, utils
 
-IS_SAMPLE_EXP = False
+IS_SAMPLE_EXP = True
 
 
 def main():
@@ -22,10 +23,75 @@ def main():
     """
     # 加载数据
     print('加载训练数据。。。。')
-    X_train, y = utils.load_fashion_mnist_data(config.train_data_file)
-    X_test, y = utils.load_fashion_mnist_data(config.test_data_file)
+    X_train, y_trian = utils.load_fashion_mnist_data(config.train_data_file)
+    X_test, y_test = utils.load_fashion_mnist_data(config.test_data_file)
 
-    print(type(X_test))
+    # print(type(X_test))
+
+    # 随机查看9张图片
+    utils.polt_random_samples(X_train)
+
+    # 训练数据特征提取
+    print('训练数据特征提取。。。。')
+    feats_train = utils.extract_feats(X_train)
+    # 测试数据特征提取
+    print('测试数据特征提取。。。。')
+    feats_test = utils.extract_feats(X_test)
+
+    # 特征归一化处理
+    proc_feats_train, pro_feats_test = utils.do_feature_engineering(feats_train, feats_test)
+
+    # 数据建模及验证
+    print('\n===================== 数据建模及验证 =====================')
+    if IS_SAMPLE_EXP:
+        # 耗时比较短，简单测试
+        print('简单的Logistic Regression分类：')
+        lf = LogisticRegression()
+        lf.fit(proc_feats_train, y_trian)
+        print('测试准确率{}'.format(lf.score(pro_feats_test, y_test)))
+    else:
+        # 耗时比较长
+        print('多个模型交叉验证分类比较：')
+        model_name_param_dict = {'KNN' : (KNeighborsClassifier(),
+                                        {'n_neighbors' : [5, 15, 20]}),
+                                 'LR' : (LogisticRegression(),
+                                         {'C' : [0.01, 1, 10]}),
+                                 'SVM' : (SVC(),
+                                          {'C' : [0.01, 0.1, 1, 10]}),
+                                 'DT' : (DecisionTreeClassifier(),
+                                         {'max_depth' : [25, 55, 95]}),
+                                 'Adaboost' : (AdaBoostClassifier(),
+                                               {'n_estimators' : [100, 150, 200]}),
+                                 'GBDT' : (GradientBoostingClassifier(),
+                                           {'learning_rate' : [0.01, 1, 10]}),
+                                 'RF' : (RandomForestClassifier(),
+                                         {'n_estimators' : [100, 200, 250]})
+                                 }
+        # 比较结果的DataFrame
+        result_df = pd.DataFrame(columns=['Accuracy (%)', 'Time (s)'],
+                                 index=list(model_name_param_dict.keys()))
+        result_df.index.name = 'model'
+
+        for model_name, (model, param_range) in model_name_param_dict.items():
+            best_clf, best_acc, mean_duration = utils.train_and_test_model(proc_feats_train, y_trian,
+                                                                           pro_feats_test, y_test,
+                                                                           model_name, model, param_range)
+            result_df.loc[model_name, 'Accuracy (%)'] = best_acc * 100
+            result_df.loc[model_name, 'Time (s)'] = mean_duration
+        result_df.to_csv(os.path.join(config.output_path, 'model_comparison.csv'))
+
+        # 模型及结果比较
+        #         print('\n===================== 模型及结果比较 =====================')
+        plt.figure(figsize=(10, 5))
+        ax1= plt.subplot(1, 2, 1)
+        result_df.plot(y=['Accuracy (%)'], kind = 'bar', ylim=[50, 100], ax= ax1, title='Accuracy (%)', legend=False)
+
+        ax2 = plt.subplot(1, 2, 2)
+        result_df.plot(y=['Time (s)'], kind='bar', ax=ax2, title='Time (s)', legend=False)
+        plt.tight_layout()
+        plt.savefig(os.path.join(config.output_path, 'pred_results.png'))
+        plt.show()
+
 
 if __name__ == '__main__':
     main()
--- PythonAI/ImageData/FashionMnist/utils.py
+++ PythonAI/ImageData/FashionMnist/utils.py
@@ -10,12 +10,12 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-# import cv2
-# from cv2 import *
+import cv2
 from sklearn.preprocessing import StandardScaler
 import time
 from sklearn.model_selection import GridSearchCV
-from PythonAI.ImageData.FashionMnist import config
+from PythonAI.ImageData.FashionMnist import config, utils
+
 
 def load_fashion_mnist_data(datafile):
     """
@@ -27,8 +27,100 @@ def load_fashion_mnist_data(datafile):
                     y:  标签
     """
     data_df = pd.read_csv(datafile)
-    print(data_df)
+    # print(data_df.iloc[2, :])
     X = data_df.iloc[:, 1:].values.astype(np.uint8)
     y = data_df.iloc[:, 0].values.astype(np.uint8)
     print('共有{}个图像'.format(X.shape[0]))
     return X, y
+
+
+def polt_random_samples(X):
+    """
+        随机选取9张图像数据进行可视化
+        参数：
+            :param X: 数据矩阵[n_samples, img_rows * img_cols]
+    """
+    random_X = X[np.random.choice(X.shape[0], 9, replace=False), :]
+    for i in range(9):
+        img_data = random_X[i, :].reshape(config.img_rows, config.img_cols)
+        plt.subplot(3, 3, i+1)
+        plt.imshow(img_data, cmap='gray')
+        plt.tight_layout()
+    # plt.show()
+
+
+def extract_feats(X):
+    """
+        特征提取：
+        参数：
+            :param X: 数据矩阵[n_samples, img_rows * img_cols]
+            :return: -feat_arr: 特征矩阵
+    """
+    n_sample = X.shape[0]
+    feat_list = []
+    for i in range(n_sample):
+        img_data = X[i, :].reshape(config.img_rows, config.img_cols)
+        # 中值滤波， 去除噪声
+        blur_img_data = cv2.medianBlur(img_data, 3)
+
+        # 直方图均衡化
+        equ_blur_img_data = cv2.equalizeHist(blur_img_data)
+
+        # 将图像转换成特征向量
+        feat = equ_blur_img_data.flatten()
+        feat_list.append(feat)
+
+        if (i + 1) % 5000 ==0:
+            print('已完成{}个特征的提取。'.format(i + 1))
+    feat_arr = np.array(feat_list)
+    return feat_arr
+
+def do_feature_engineering(feats_train, feats_test):
+    """
+        特征处理
+        参数
+            :param feats_train: 训练数据特征矩阵
+            :param feats_test:  测试数据特征矩阵
+        :return:
+
+
+    """
+    std_scaler = StandardScaler()
+    scaled_feats_train = std_scaler.fit_transform(feats_train.astype(np.float64))
+    scaled_feats_test = std_scaler.transform(feats_test.astype(np.float64))
+
+    return scaled_feats_train, scaled_feats_test
+
+
+def train_and_test_model(X_train, y_trian, X_test, y_test, model_name, model, param_range):
+    """
+        根据给定模型并返回
+            1.最优模型
+            2.平均训练耗时
+            3.准确率
+            :param X_train:
+            :param y_trian:
+            :param X_test:
+            :param y_test:
+            :param model_name:
+            :param model:
+            :param param_range:
+        :return:
+    """
+    print('训练{}。。。'.format(model_name))
+    clf = GridSearchCV(estimator=model, param_grid=param_range,
+                       cv=3, scoring='accuracy', refit=True)
+    # 计时
+    start = time.time()
+    clf.fit(X_train, y_trian)
+    end = time.time()
+    duration = end - start
+    print('{}模型耗时{:.4f}s'.format(model_name, duration))
+
+    # 验证模型
+    print('训练数据准确率：{:.3f}'.format(clf.score(X_train, y_trian)))
+
+    score = clf.score(X_test, y_test)
+    print('测试数据准确率{:.3f}'.format(score))
+
+    return clf, score, duration
