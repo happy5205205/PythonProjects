9a0ea17517786aade95b370b54dc310f2f15cbf6
happy5205205 <30718405+happy5205205@users.noreply.github.com>
2019-01-03 13:56:30 +0800

    增加leetcode提高代码能力 2.两数相加

--- leetcode/2.两数相加.py
+++ leetcode/2.两数相加.py
@@ -12,6 +12,11 @@
     输出：7 -> 0 -> 8
     原因：342 + 465 = 807
 """
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def addTwoNumbers(self, l1, l2):
@@ -20,7 +25,45 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        # 这里开始不做L1，L2非空判断，题义表面名为非空链表
-        # 记录是否需要增加新节点，或在链表下一个节点时候需要加1，同时记录链表同级节点的和
+        # 这里一开始不做l1、l2非空判断，题意表明非空链表
+        # 记录是否需要增加新节点，或在链表下一个节点是否需要加1，同时记录链表同级节点的和
         carry = 0
-        # 这里执行的顺序是res = ListNode(0),pre = res
+        # 这里的执行顺序是res = ListNode(0), pre = res
+        res = pre = ListNode(0)
+        # 判断l1、l2、carry是否有值，carry有值的话需要增加新节点，或在链表下一个节点需要加1
+        while l1 or l2 or carry:
+            # 判断l1是否有值
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            # 判断l2是否有值
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            # carry有同级节点的和
+            # divmod返回商与余数的元组，拆包为carry和val
+            # carry有值的话需要增加新节点，或在链表下一个节点需要加1,在循环中会用到
+            carry, val = divmod(carry, 10)
+            # 新建链表节点
+            # 这里是n.next = ListNode(val), n = n.next()
+            pre.next = pre = ListNode(val)
+        # res等价于pre，res.val=0，所以返回res.next
+        return res.next
+
+if __name__ == '__main__':
+    # 创建对象Solution
+    sol = Solution()
+    # 定义l1链表
+    l1 = ListNode(2)
+    l1.next = l11 = ListNode(3)
+    l11.next = l12 = ListNode(4)
+    # 定义l2链表
+    l2 = ListNode(5)
+    l2.next = l21 = ListNode(6)
+    l21.next = l22 = ListNode(4)
+    # 获取返回值的链表
+    res = sol.addTwoNumbers(l1, l2)
+    # 循环遍历出来
+    while res:
+        print(res.val)
+        res = res.next
